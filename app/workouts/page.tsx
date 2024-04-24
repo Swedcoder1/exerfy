85a08acf5c0c1dc6dad14a69c2260408eb381c7a
swedcoder <swedcoder@gmail.com>
2024-04-24 21:37:30 +0200

    Added little functionality

--- app/workouts/page.tsx
+++ app/workouts/page.tsx
@@ -1,10 +1,11 @@
-// import connectDB from "../lib/connect-db";
-// import Workout from "../schemas/mongoose/workout";
 import { supabase } from "../utils/supabase/supabaseClient";
+import { LuDumbbell } from "react-icons/lu";
 
 export default async function Page() {
   try {
-    const { data: notes, error } = await supabase.from("notes").select("*");
+    const { data: workouts, error } = await supabase
+      .from("workouts")
+      .select("*");
 
     if (error) {
       console.error("Error fetching notes:", error);
@@ -13,11 +14,32 @@ export default async function Page() {
 
     return (
       <>
-        <div className="flex justify-center mt-10 items-center">
-          <h1 className="text-center text-2xl">Your workouts</h1>
-          <button className="btn btn-sm btn-primary">Create workout</button>
+        <div className=" text-center mt-10">
+          <h1 className="text-2xl flex-grow">Your workouts</h1>
+          <button className="btn btn-sm btn-primary mt-2">
+            Create workout
+          </button>
         </div>
-        <pre>{JSON.stringify(notes, null, 2)}</pre>
+
+        <div>
+          {workouts.length == 0 ? (
+            <div className="flex flex-col justify-center items-center h-screen">
+              <LuDumbbell className="text-5xl mb-4" />
+              <p className="text-center">
+                Oops! Looks like you don't have any workouts yet.
+              </p>
+            </div>
+          ) : (
+            <div>
+              {workouts.map((workout) => (
+                <div>
+                  <p>{workout.title}</p>
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
+        {/* <pre>{JSON.stringify(workouts, null, 2)}</pre> */}
       </>
     );
   } catch (error) {
